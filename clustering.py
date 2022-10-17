bc004af8640b738061a118dda445eec062f3db00
Vinit Jogani <vnjogani@gmail.com>
2022-10-17 23:28:31 +0400

    Initial commit

---
+++ clustering.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.mixture import GaussianMixture
+
+from datasets import *
+
+
+def train_algo(dataset, algo):
+    X, _ = load_dataset(dataset)
+    n_clusters = [2, 4, 8, 16, 32, 48, 64]
+    out = {}
+    for k in n_clusters:
+        model = algo(k)
+        y_pred = model.fit_predict(X)
+        score = calinski_harabasz_score(X, y_pred)
+        out[k] = (model, score)
+        print(k, score)
+    return out
+
+
+def train_kmeans(dataset):
+    path = f"readings/kmeans_{dataset}.pkl"
+    if os.path.exists(path):
+        pickle.load(open(path, "rb"))
+    out = train_algo(dataset, KMeans)
+    pickle.dump(out, open(path, "wb"))
+    return out
+
+
+def train_gmm(dataset):
+    path = f"readings/gmm_{dataset}.pkl"
+    if os.path.exists(path):
+        pickle.load(open(path, "rb"))
+    out = train_algo(dataset, GaussianMixture)
+    pickle.dump(out, open(path, "wb"))
+    return out
+
+
+if __name__ == "__main__":
+    for dataset in ["credit_score", "term_deposits"]:
+        print(dataset)
+        train_kmeans(dataset)
+        train_gmm(dataset)
